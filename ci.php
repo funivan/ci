f76906ee6619498e4c54a47fbb58a1a345d39dda
Ivan Scherbak <dev@funivan.com>
2016-07-05 12:47:11 +0300

    add ci config file

---
+++ ci.php
@@ -0,0 +1,15 @@
+<?php
+
+  use App\Ci\Commands\MailCommand;
+  use App\Ci\Commands\ShellCommand;
+
+  /** @var \App\Ci\Configuration\RunConfig $runConfig */
+
+  $config = new \App\Ci\Configuration\BaseConfig();
+
+  $testProfile = $config->createProfile('test');
+
+  $testProfile->addCommand(new ShellCommand('composer install'));
+  $testProfile->addCommand(new ShellCommand('./vendor/bin/phpunit --stop-on-error'));
+
+  $testProfile->onFailure(new MailCommand($commit->author_email, 'Build failure', 'See:' . $runConfig->getCommit()->getUrl()));
