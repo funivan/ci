cd33dceae1b669aa4eefc159e5247c09742d7519
Ivan Scherbak <dev@funivan.com>
2016-07-05 13:37:12 +0300

    create test db for phpunit

--- Tests/TestCase.php
+++ Tests/TestCase.php
@@ -1,6 +1,8 @@
 <?php
   namespace Tests;
 
+  use Illuminate\Support\Facades\DB;
+
   class TestCase extends \Illuminate\Foundation\Testing\TestCase {
 
     /**
@@ -18,8 +20,25 @@
      */
     public function createApplication() {
       $app = require __DIR__ . '/../bootstrap/app.php';
+      /** @var \Illuminate\Foundation\Application $app */
 
-      $app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+      /** @var \Illuminate\Contracts\Console\Kernel $kernel */
+      $kernel = $app->make(\Illuminate\Contracts\Console\Kernel::class);
+      /** @noinspection PhpUndefinedMethodInspection */
+      $kernel->bootstrap();
+
+
+      if (DB::connection() instanceof \Illuminate\Database\SQLiteConnection) {
+        $database = DB::connection()->getDatabaseName();
+        if (file_exists($database)) {
+          unlink($database);
+        }
+
+        touch($database);
+        DB::reconnect(config('database.default'));
+      }
+
+      $kernel->call('migrate');
 
       return $app;
     }
